d0a04ce80ef0d60fa7f88776a128ca3923c20c4a
Abi-Success <aabigail@st.ug.edu.gh>
2024-06-07 21:13:33 +0000

    Second Component

--- myapp/App.js
+++ myapp/App.js
@@ -1,5 +1,5 @@
-import{SafeAreaView, View,Image, Text, TouchableOpacity} from 'react-native';
-
+import{SafeAreaView, View,Image, Text, TouchableOpacity, TextInput} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 export default function App() {
@@ -13,6 +13,7 @@ export default function App() {
       borderRadius: 40,
       }}>
 
+      {/*First Component starts here*/}
       <View style={{
         display: "flex",
         flexDirection: "row",
@@ -34,10 +35,50 @@ export default function App() {
           padding:6.5,
           borderRadius: 30,}}>
           <Image source={require("./assets/person.png")}></Image>
+         </TouchableOpacity>
+      </View> 
+      {/* And ends here*/}
+      
+      {/**  Second Component starts from here */}
+      <View style={{
+        display: "flex", 
+        flexDirection: "row", 
+        alignItems: "center", 
+        justifyContent: "space-between",
+        width: 380,
+        height: 52,
+        top: 48,
+        left: 2, }}>
 
+        <TouchableOpacity>
+          <Icon  style={{
+            fontSize: 20,
+            borderRadius: 10, 
+            padding: 14,  
+            display: 'flex', 
+            alignItems: 'flex',
+            backgroundColor: "white",}} name='search'/>
         </TouchableOpacity>
-      </View>
 
+        <TextInput style={{
+          display: 'flex',
+          alignItems: 'flex',
+          backgroundColor: "white",
+          width: 285,
+          height: 48,
+          borderRadius: 10,
+          padding: 5,
+          right: 4.5,
+          }} placeholder="Search">
+        </TextInput>
+        <View style={{
+          backgroundColor: "#F0522F", 
+          padding: 10, 
+          borderRadius: 10}}>
+          <Image source={require("./assets/vector.png")} />
+        </View>
+      </View>
+       {/*And ends here*/}
     </View>
   </SafeAreaView>
   )
